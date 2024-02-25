e224a2cef1607282a1eeb2d60c41333446d247d2
AbigailWilliams1 <2123371072@qq.com>
2024-02-25 19:44:45 +0800

    20240225 daily one problem

---
+++ exercise/235.cpp
@@ -0,0 +1,31 @@
+/*
+235. 二叉搜索树的最近公共祖先               难度：中等
+给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
+百度百科中最近公共祖先的定义为：
+    “对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+例如，给定如下二叉搜索树:  root = [6,2,8,0,4,7,9,null,null,3,5]
+
+链接：https://leetcode.cn/problems/lowest-common-ancestor-of-a-binary-search-tree/
+*/
+#include<algorithm>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (p->val > q->val)swap(p, q);
+        if (root->val < p->val)return lowestCommonAncestor(root->right, p, q);
+        if (root->val > q->val)return lowestCommonAncestor(root->left, p, q);
+        return root;
+    }
+};
